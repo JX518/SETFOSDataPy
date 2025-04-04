305d9778f414fc2e3e511d1b2fae02bfdc27aec9
Justin Xu <xujustin7@gmail.com>
2025-04-04 16:03:15 -0700

    auto push script

--- gitpush.sh
+++ gitpush.sh
@@ -3,6 +3,7 @@ cd /home/xujus/Documents/ASU/Research/git
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github
 ssh -T git@github.com
+git remote add configs git@github.com:JX518/SETFOSDataPy.git
 git branch -M main
 git add /home/xujus/Documents/ASU/Research/git/*
 git commit
